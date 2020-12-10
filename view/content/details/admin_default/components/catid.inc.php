65920e0994e97a6a94bf95bf764b11f939c532d0
Jayin Ton <tonjayin@gmail.com>
2020-12-10 15:52:45 +0800

    改进catid 组件

--- view/content/details/admin_default/components/catid.inc.php
+++ view/content/details/admin_default/components/catid.inc.php
@@ -1,8 +1,17 @@
 <script type="text/x-template" id="default-catid">
     <div class="default-catid">
-        <el-form-item :label="field.name" required>
-            <el-input v-model="field_data[field.field]" :placeholder="'请输入'+field.name" clearable :style="{width: '100%'}"  @change="handleChange" disabled>
-            </el-input>
+        <el-form-item v-if="loaded" :label="field.name" required>
+            <el-select v-model="result" placeholder="请选择">
+                <el-option
+                        v-for="item in categoryList"
+                        :key="item.catid"
+                        :label="item.catname"
+                        :value="item.catid">
+                    <template v-for="i in item.level * 2"><span>&nbsp;</span></template>
+                    <template v-if="item.level > 0"><span> ∟</span></template>
+                    <span>{{ item.catname }}</span>
+                </el-option>
+            </el-select>
         </el-form-item>
     </div>
 </script>
@@ -11,38 +20,64 @@
 <script>
     $(function () {
         Vue.component('default-catid', {
+            template: '#default-catid',
+            model: {
+                // 指定接收 v-model 值/修改事件
+                prop: 'field_value',
+                event: 'change'
+            },
             props: {
                 field: {
                     type: Object,
-                    defalut :function() {
+                    defalut: function () {
                         return {};
                     }
                 },
-                field_data : {
+                field_data: {
                     type: Object,
-                    defalut : function() {
+                    defalut: function () {
                         return {};
+                    }
+                },
+                field_value: {
+                    defualt: 0
+                }
+            },
+            watch: {},
+            computed: {
+                result: {
+                    get: function () {
+                        return Number(this.field_value)
+                    },
+                    set: function (val) {
+                        // 调用父组件的customFunc方法，将变量放在第二个参数，即可改变父组件的变量
+                        this.$emit('change', val)
                     }
                 }
             },
-            watch: {
-
-            },
-            template: '#default-catid',
-            data: function() {
+            data: function () {
                 return {
-
+                    // 是否已请求完列表数据
+                    loaded: false,
+                    categoryList: []
                 }
             },
-            created: function() {
-
+            created: function () {
             },
-            mounted:function() {
-
+            mounted: function () {
+                this.getCategoryList()
             },
             methods: {
-                handleChange :function(code) {
-
+                handleChange: function (code) {
+                },
+                getCategoryList: function () {
+                    var that = this
+                    this.httpGet("{:api_url('/cms/Content/details')}", {_action: 'getCategoryList'}, function (res) {
+                        if (res.status) {
+                            that.categoryList = res.data
+                            that.loaded = true
+                        }
+                    })
                 }
             }
         });
